68ce046949c618e495693195842271fe2f1a2128
JeiHO <chechomancr@gmail.com>
2024-08-30 17:25:26 -0600

    Update scripts.php

--- src/scripts.php
+++ src/scripts.php
@@ -2,7 +2,7 @@
 
 if (class_exists('Leaf\Config')) {
     \Leaf\Config::addScript(function ($app) {
-        \Leaf\Config::singleton('logWriter', static fn($c) => is_object($c['log.writer']) ?
+        \Leaf\Config::singleton('logWriter', fn($c) => is_object($c['log.writer']) ?
             $c['log.writer'] :
             new \Leaf\LogWriter($c['log.dir'] . $c['log.file'], $c['log.open'] ?? true));
 
