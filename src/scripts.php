27bde5999ea341efe0debeef98ce168fe3afd007
JeiHO <chechomancr@gmail.com>
2024-08-30 17:23:26 -0600

    Use \Leaf\Config::singleton instead of $app->register

--- src/scripts.php
+++ src/scripts.php
@@ -2,9 +2,11 @@
 
 if (class_exists('Leaf\Config')) {
     \Leaf\Config::addScript(function ($app) {
-        $app->register('logWriter', fn($c) => is_object($c['log.writer']) ? $c['log.writer'] : new \Leaf\LogWriter($c['log.dir'] . $c['log.file'], $c['log.open'] ?? true));
+        \Leaf\Config::singleton('logWriter', static fn($c) => is_object($c['log.writer']) ?
+            $c['log.writer'] :
+            new \Leaf\LogWriter($c['log.dir'] . $c['log.file'], $c['log.open'] ?? true));
 
-        $app->register('log', function ($c) {
+        \Leaf\Config::singleton('log', function ($c) {
             $log = new \Leaf\Log(\Leaf\Config::get("logWriter"));
             $log->enabled($c['log.enabled']);
             $log->level($c['log.level']);
