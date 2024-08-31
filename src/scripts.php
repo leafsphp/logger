a99608f65e922766485cf07d8f6d3c1a1d16d273
JeiHO <chechomancr@gmail.com>
2024-08-30 18:42:47 -0600

    Variable that is not in use is removed

--- src/scripts.php
+++ src/scripts.php
@@ -1,7 +1,7 @@
 <?php
 
 if (class_exists('Leaf\Config')) {
-    \Leaf\Config::addScript(function ($app) {
+    \Leaf\Config::addScript(function () {
         \Leaf\Config::singleton('logWriter', fn($c) => is_object($c['log.writer']) ?
             $c['log.writer'] :
             new \Leaf\LogWriter($c['log.dir'] . $c['log.file'], $c['log.open'] ?? true));
