fb1a77e611a31ca7d0bb03090c65e991f3cc5107
Michael Darko <mickdd22@gmail.com>
2024-10-02 14:47:27 +0000

    fix: switch to PHP 7 syntax

--- src/scripts.php
+++ src/scripts.php
@@ -2,9 +2,11 @@
 
 if (class_exists('Leaf\Config')) {
     \Leaf\Config::addScript(function () {
-        \Leaf\Config::singleton('logWriter', fn($c) => is_object($c['log.writer']) ?
-            $c['log.writer'] :
-            new \Leaf\LogWriter($c['log.dir'] . $c['log.file'], $c['log.open'] ?? true));
+        \Leaf\Config::singleton('logWriter', function ($c) {
+            is_object($c['log.writer']) ?
+                $c['log.writer'] :
+                new \Leaf\LogWriter($c['log.dir'] . $c['log.file'], $c['log.open'] ?? true);
+        });
 
         \Leaf\Config::singleton('log', function ($c) {
             $log = new \Leaf\Log(\Leaf\Config::get("logWriter"));
