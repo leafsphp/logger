b9c0f446b7d95120a266c9b87b08352690dd48f2
Isaac <84479744+isaac-araujo@users.noreply.github.com>
2024-10-11 10:08:16 -0300

    Fix: missing return statement in logWriter configuration closure

--- src/scripts.php
+++ src/scripts.php
@@ -3,7 +3,7 @@
 if (class_exists('Leaf\Config')) {
     \Leaf\Config::addScript(function () {
         \Leaf\Config::singleton('logWriter', function ($c) {
-            is_object($c['log.writer']) ?
+            return is_object($c['log.writer']) ?
                 $c['log.writer'] :
                 new \Leaf\LogWriter($c['log.dir'] . $c['log.file'], $c['log.open'] ?? true);
         });
