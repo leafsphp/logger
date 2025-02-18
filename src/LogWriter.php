a580e81d01f0009be3dbc2e8f3255bcdecc0f242
mychidarko <mickdd22@gmail.com>
2025-02-18 19:45:03 +0000

    fix: add recursive log creation

--- src/LogWriter.php
+++ src/LogWriter.php
@@ -25,9 +25,7 @@ class LogWriter
     {
         if (!file_exists($file)) {
             if ($createFile) {
-                FS\File::create($file, null, [
-                    'recursive' => true,
-                ]);
+                FS\File::create($file, null, ['recursive' => true]);
             } else {
                 trigger_error(basename($file) . " not found in " . dirname($file), E_USER_ERROR);
             }
